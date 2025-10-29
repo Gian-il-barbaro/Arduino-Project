0a4f79023ab5092ef1e41a7fe075026b6f580306
Gian il Bradipo <47137704+Gian-il-barbaro@users.noreply.github.com>
2025-10-29 17:23:31 +0100

    Refactor game logic and LED control in Maincode.ino

--- Arduino/Maincode/Maincode.ino
+++ Arduino/Maincode/Maincode.ino
@@ -1,3 +1,12 @@
+const int NUM_DIGITS = 3;
+const int MAX_ATTEMPTS = 6;
+
+String soluzione;
+int currentAttemptCount = 0;
+bool gameActive = false;
+
+
+//---- variabili per led ----
 #define red1 3      // led 1 colore rosso
 #define green1 2    // led 1 colore verde
 #define red2 4      // led 2 colore rosso
@@ -9,27 +18,16 @@
 #define numLed 3  // numero di led
 
 #define BASE 10  // numeri in base 10
-
-String soluzione;  // la soluzione
-String input;      // l'input
 int redPin;        // pin del colore rosso di un led
 int greenPin;      // pin del colore verde di un led
 
-// int countersInput[BASE];	// per contare quante volte le cifre da 0 a 9 compaiono nell'input
 int countersSol[BASE];  // per contare quante volte le cifre da 0 a 9 compaiono nella soluzione
-
-int cInput;  // l'i-esimo carattere della stringa input
-
 int valutati[BASE];  // il numero di cifre dell'input già valutate
-
-int x;             // per testare il funzionamento del codice, nella versione definitiva va eliminata
-int combinazioni;  // numero di combinazioni possibili
+int cInput;   // l'i-esimo carattere della stringa input
 
 void setup() {
-
   Serial.begin(9600);
-
-  // imposto tutti i pin di tutti i led in modalità OUTPUT
+   // imposto tutti i pin di tutti i led in modalità OUTPUT
   pinMode(red1, OUTPUT);
   pinMode(green1, OUTPUT);
   // pinMode(blue1, OUTPUT);
@@ -43,63 +41,92 @@ void setup() {
   // imposto il pin del piezo in modalità OUTPUT
   pinMode(piezo, OUTPUT);
   digitalWrite(piezo, LOW);
-
-  // per permettere al metodo random() di generare una soluzione casuale
+  
+  switchOffAllLEDs();
+  
+  while (!Serial) {
+    ; // Attendi che la seriale sia pronta
+  }
+  
   randomSeed(analogRead(0));
-
-  x = 123;
-  combinazioni = (int)(pow(BASE, numLed));
+  
+  Serial.println("ARDUINO: Pronto per gioco Wordle");
+  Serial.println("ARDUINO: In attesa di GAME_START...");
 }
 
 void loop() {
+  if (Serial.available()) {
+    String command = Serial.readStringUntil('\n');
+    command.trim();
+    processCommand(command);
+  }
+}
 
-  soluzione = String(random(combinazioni));
-  while (soluzione.length() < numLed)
-    soluzione = "0" + soluzione;
+void processCommand(String command) {
+  //Serial.println("ARDUINO: Ricevuto: " + command);
+  
+  if (command == "GAME_START") {
+    startNewGame();
+  }
+  else if (command.startsWith("TRY:")) {
+    if (!gameActive) {
+      Serial.println("ERROR: Gioco non attivo");
+      return;
+    }
+    
+    String attempt = command.substring(4);
+    //if (attempt.length() == NUM_DIGITS && isDigits(attempt)) {
+    processAttempt(attempt);
+    //} else {
+      //Serial.println("ERROR: Tentativo non valido");
+    //}
+  }
+}
 
-  soluzione = "123";
+void startNewGame() {
+
+  switchOffAllLEDs();
+
+  // Genera una soluzione casuale di 3 cifre
+  soluzione = "";
+  
+  for (int i = 0; i < NUM_DIGITS; i++) {
+    soluzione += String(random(0, 10));
+  }
 
   for (int i = 0; i < BASE; i++)
     countersSol[i] = 0;
+    
+
   for (int i = 0; i < soluzione.length(); i++)
     // conta quante occorrenze di ogni cifra ci sono nella soluzione
     countersSol[(int)(soluzione.charAt(i)) - 48]++;
 
-  // Serial.println(soluzione);
+  currentAttemptCount = 0;
+  gameActive = true;
+  
+  Serial.println("ARDUINO: Nuova partita - Soluzione: " + soluzione);
+  Serial.println("GAME_START");
+}
 
-  do {
-    // input = Serial.readString();
-    // inserisci numero
-    x = x % combinazioni;
-    input = String(x);
-    while (input.length() < numLed)
-      input = "0" + input;
-    // input = "122";
-    Serial.println("Sol: " + soluzione + "\tIn: " + input);
-    // Serial.println("In: " + input);
-
-    /*for(int i=0; i<BASE; i++)
-      // tutti i contatori dell'input vengono inzializzati a 0
-      countersInput[i] = 0;
-    
-    for(int i=0; i<input.length(); i++)
-      // tutti i contatori dell'input assumono un valore
-      countersInput[(int)(input.charAt(i))-48]++;*/
-
-  } while (input.length() != numLed || !isInteger(input));
-  // l'input deve essere lungo esattamente quanto la soluzione e deve contenere solo cifre
-
+void processAttempt(String attempt) {
   for (int i = 0; i < BASE; i++)
     valutati[i] = 0;
+  currentAttemptCount++;
+  
+//  Serial.println("ARDUINO: Tentativo " + String(currentAttemptCount) + ": " + attempt);
+  
+  // Calcola i colori per ogni cifra
+  String colors[NUM_DIGITS];
+ // bool usedInSolution[NUM_DIGITS] = {false, false, false};
 
-  // prima passata:
-  // accendo solamente i led verdi e conto quanti ne ho accesi
-  for (int i = 0; i < soluzione.length(); i++) {
 
-    if (soluzione.charAt(i) == input.charAt(i)) {
-      // se l'i-esimo carattere della soluzione e dell'input coincidono
-
-      cInput = (int)(input.charAt(i)) - 48;
+  // Prima passata: cifre corrette nella posizione giusta (right)
+  for (int i = 0; i < NUM_DIGITS; i++) {
+    if (attempt[i] == soluzione[i]) {
+      cInput = (int)(attempt.charAt(i)) - 48;
+      colors[i] = "right";
+      valutati[cInput]++; // incremento il numero di cInput valutati
 
       // faccio capire al sistema quale led deve accendere
       switch (i) {
@@ -119,67 +146,95 @@ void loop() {
 
       led(redPin, greenPin, 0, 255);
       // accendo il led di verde
-
-      valutati[cInput]++;
-      // e incremento il numero di cInput valutati
-    }
-  }
-
-  // seconda passata:
-  // se il led non è stato acceso di verde
-  for (int i = 0; i < soluzione.length(); i++) {
-
-    if (soluzione.charAt(i) != input.charAt(i)) {
-      // se l'i-esimo carattere della soluzione e dell'input non coincidono
-
-      cInput = (int)(input.charAt(i)) - 48;
-
-      // faccio capire al sistema quale led deve accendere
-      switch (i) {
-        case 0:
-          redPin = red1;
-          greenPin = green1;
-          break;
-        case 1:
-          redPin = red2;
-          greenPin = green2;
-          break;
-        case 2:
-          redPin = red3;
-          greenPin = green3;
-          break;
-      }
-
-      if (/*contains(soluzione, input.charAt(i)) && */ valutati[cInput] < countersSol[cInput])
-        // se l'i-esimo carattere è presente altre volte nella soluzione
-
-        led(redPin, greenPin, 255, 127);
-        // accendo il led di arancione
-        
-      else
-        // se l'i-esimo carattere non è presente altre volte nella soluzione
-
-        led(redPin, greenPin, 255, 0);
-        // accendo il led di rosso
-
-      valutati[cInput]++;
-      // incremento il numero di cInput valutati
     }
   }
   
-  if (soluzione == input) {
-    // se l'input e la soluzione coincidono, faccio suonare il piezo
+  // Seconda passata: cifre corrette ma in posizione sbagliata (wrong)
+  for (int i = 0; i < NUM_DIGITS; i++) {
+    if (colors[i] == "right") continue; // Salta quelle già segnate come right
+    cInput = (int)(attempt.charAt(i)) - 48;
+    // faccio capire al sistema quale led deve accendere
+    switch (i) {
+      case 0:
+        redPin = red1;
+        greenPin = green1;
+        break;
+      case 1:
+        redPin = red2;
+        greenPin = green2;
+        break;
+      case 2:
+        redPin = red3;
+        greenPin = green3;
+        break;
+    }
+    if (valutati[cInput] < countersSol[cInput]) {
+      colors[i] = "wrong";
+      led(redPin, greenPin, 255, 127);
+        // accendo il led di arancione
+    }
+    else{
+      // se l'i-esimo carattere non è presente altre volte nella soluzione
+      led(redPin, greenPin, 255, 0);
+      // accendo il led di rosso
+      colors[i]= "empty";
+    }
+    valutati[(int)(attempt[i])]++;
+  }
+  
+  // Costruisce la stringa di colori
+  String colorString = "COLORS:";
+  for (int i = 0; i < NUM_DIGITS; i++) {
+    if (i > 0) colorString += ",";
+    colorString += colors[i];
+  }
+  
+  Serial.println(colorString);
+  
+  // DEBUG: Stampa i colori per verifica
+  //Serial.print("DEBUG Colors: ");
+  for (int i = 0; i < NUM_DIGITS; i++) {
+    Serial.print(colors[i]);
+    Serial.print(" ");
+  }
+  Serial.println();
+  
+  // Controlla vittoria/sconfitta
+  bool isWin = true;
+  for (int i = 0; i < NUM_DIGITS; i++) {
+    if (colors[i] != "right") {
+      isWin = false;
+      break;
+    }
+  }
+  
+  if (isWin) {
+    Serial.println("GAME_OVER:WIN");
+    gameActive = false;
+    Serial.println("ARDUINO: VITTORIA!");
     for (int i = 0; i < 3; i++) {
       tone(piezo, 1000, 100);
       delay(250);
     }
-  }
-  else
     noTone(piezo);
-
-  delay(2 * 1000);
-  x++;
+  }
+  
+  else if (currentAttemptCount >= MAX_ATTEMPTS) {
+    Serial.println("GAME_OVER:LOSE");
+    gameActive = false;
+    Serial.println("ARDUINO: SCONFITTA - Soluzione: " + soluzione);
+  }
 }
+
+/*bool isDigits(String str) {
+  for (int i = 0; i < str.length(); i++) {
+    if (!isDigit(str[i])) {
+      return false;
+    }
+  }
+  return true;
+}*/
+
 
 /**
 	accende un led specifico:
@@ -189,7 +244,7 @@ void loop() {
 */
 void led(int redPin, int greenPin, int red, int green) {
 
-  /*if (redPin == red1) {
+  /*if (redPin == red2) {
     red = 255 - red;
     green = 255 - green;
   }*/
@@ -199,29 +254,10 @@ void led(int redPin, int greenPin, int red, int green) {
 }
 
 /**
-	data una stringa s, restituisce true se s può essere espressa tramite un numero intero,
-    false altrimenti
+  spegne tutti i led
 */
-bool isInteger(String s) {
-
-  for (int i = 0; i < s.length(); i++) {
-    if (s.charAt(i) < '0' || s.charAt(i) > '9')
-      return false;
-  }
-  return true;
+void switchOffAllLEDs(){
+  led(red1, green1, 0, 0);
+  led(red2, green2, 255, 255);
+  led(red3, green3, 0, 0);
 }
-
-/** 
-	metodo bool;
-    restituisce true se la stringa s contiene il carattere c,
-    false altrimenti
-*/
-/*bool contains(String s, char c){
-  
-  for(int i=0; i<s.length(); i++){
-    if(s.charAt(i) == c)
-      return true;
-  }
-  return false;
-  
-}*/
